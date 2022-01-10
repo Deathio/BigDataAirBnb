6b84ae57b48e65280e592d1ab7f09887a5dced2a
Lucas <ls.andrade2804@gmail.com>
2022-01-10 10:16:44 -0300

    cleanning the csvs

--- Main.java
+++ Main.java
@@ -3,38 +3,33 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.stream.Stream;
+import java.util.List;
 
 import src.classes.ApartmentAirBnb;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        FileWriter formatingDate = new FileWriter("src/outPutCSV/listings_review_date.csv");
-        FileWriter filteringAboveAverage = new FileWriter("src/outPutCSV/listings_gt_avg_prices.csv");
-        FileWriter filteringBellowAverage = new FileWriter("src/outPutCSV/listings_lt_avg_prices.csv");
+        FileWriter formatingDate = new FileWriter("src/outPutCSV/transform/listings_review_date.csv");
+        FileWriter filteringAboveAverage = new FileWriter("src/outPutCSV/transform/listings_gt_avg_prices.csv");
+        FileWriter filteringBellowAverage = new FileWriter("src/outPutCSV/transform/listings_lt_avg_prices.csv");
 
-        Stream<String> readInfo = readingInformations();
-        String[] reformatingStrings = reformatingInfo(readInfo);
+        String[] reformatingStrings = reformatingInfo(readingInformationsForTransform());
         double averageNeighbourhood = calculingTheAverage(reformatingStrings);
 
-        ApartmentAirBnb[] sortEntry = new ApartmentAirBnb[22552];
-
-        boolean passingValue = true;
+        boolean isHeadline = true;
         int okayInfos = 0, ErrorInfos = 0;
 
         for (String string : reformatingStrings) {
             try {
-                if (passingValue) {
+                if (isHeadline) {
                     formatingDate.append(string + "\n");
                     filteringAboveAverage.append(string + "\n");
                     filteringBellowAverage.append(string + "\n");
-                    passingValue = !passingValue;
+                    isHeadline = !isHeadline;
                     continue;
                 }
                 String[] getter = formatingString(string);
-                ApartmentAirBnb instantiatedInfo = new ApartmentAirBnb(getter[0], getter[1], getter[2], getter[3],
-                        getter[4], getter[5], getter[6], getter[7], getter[8], getter[9], getter[10],
-                        getter[11], getter[12], getter[13], getter[14], getter[15]);
+                ApartmentAirBnb instantiatedInfo = getApartments(getter);
 
                 formatingDate.append(instantiatedInfo.toString());
 
@@ -57,14 +52,27 @@ public class Main {
         filteringBellowAverage.close();
     }
 
-    private static Stream<String> readingInformations() throws FileNotFoundException, IOException {
+    private static List<String> readingInformationsForTransform() throws FileNotFoundException, IOException {
         try (BufferedReader readingInfo = new BufferedReader(new FileReader("src/inputCSV/listings.csv"))) {
-            return readingInfo.lines();
+            return readingInfo.lines().toList();
         }
+    }
+
+    private static List<String> readingInformationsForSort() throws FileNotFoundException, IOException {
+        try (BufferedReader readingInfo = new BufferedReader(new FileReader("src/inputCSV/listings.csv"))) {
+            return readingInfo.lines().toList();
+        }
+    }
+
+    private static ApartmentAirBnb getApartments(String[] getter) {
+        return new ApartmentAirBnb(getter[0], getter[1], getter[2], getter[3],
+                getter[4], getter[5], getter[6], getter[7], getter[8], getter[9], getter[10],
+                getter[11], getter[12], getter[13], getter[14], getter[15]);
     }
 
     private static double calculingTheAverage(String[] entryValue) {
         double averageReturn = 0;
+        int qutd = 0;
         boolean firstIgnore = true;
 
         for (String string : entryValue) {
@@ -74,12 +82,13 @@ public class Main {
             }
             String[] getter = formatingString(string);
 
-            if (getter[13].equals("")) {
+            if (!getter[13].equals("")) {
                 averageReturn += Double.parseDouble(getter[13]);
+                qutd++;
             }
         }
 
-        return averageReturn / 22552;
+        return averageReturn / qutd;
     }
 
     private static String[] formatingString(String entry) {
@@ -113,7 +122,7 @@ public class Main {
         return returnValue;
     }
 
-    private static String[] reformatingInfo(Stream<String> entryInfo) {
+    private static String[] reformatingInfo(List<String> entryInfo) {
         String[] returningString = new String[22553];
         String actualString = "";
         boolean isOkay;
